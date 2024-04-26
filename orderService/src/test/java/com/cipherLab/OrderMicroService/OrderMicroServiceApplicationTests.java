df0eef4df796c71b8aac4ad70a476543386127aa
Wajeeh Ahmed <wajeeh.ahmed000@gmail.com>
2024-04-26 14:36:35 +0500

    added orderservice

---
+++ orderService/src/test/java/com/cipherLab/OrderMicroService/OrderMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cipherLab.OrderMicroService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderMicroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
